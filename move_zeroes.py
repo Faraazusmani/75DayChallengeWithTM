fb494f757badc2586bc1b1a78feec5e25f53d9a3
Faraazusmani <60542427+Faraazusmani@users.noreply.github.com>
2022-03-24 23:36:30 +0530

    Day-1

---
+++ move_zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                x = nums[i]
+                nums.remove(nums[i])
+                nums.append(x)
